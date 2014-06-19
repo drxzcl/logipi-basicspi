3e72acbbd220f4f1fd1d53c154a490de17841c6f
drxzcl <drxzclx@gmail.com>
2014-06-19 20:26:09 +0200

    Some cleanup

--- remote_sources/_/hdl/basic_spi.vhd
+++ remote_sources/_/hdl/basic_spi.vhd
@@ -1,20 +1,5 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    14:14:22 06/21/2012 
--- Design Name: 
--- Module Name:    spartcam_beaglebone - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
+-- Basic SPI implementation. Use MODE1.
 --
 ----------------------------------------------------------------------------------
 library IEEE;
@@ -34,7 +19,6 @@ end basic_spi;
 architecture Behavioral of basic_spi is
 	
 	signal spi_value: std_logic_vector(7 downto 0);
-	--signal spi_counter: std_logic_vector(2 downto 0);
 	signal spi_readvalue: std_logic_vector(7 downto 0);
 	signal sck_synchronizer: std_logic_vector(2 downto 0);
 	signal pb0_synchronizer: std_logic_vector(2 downto 0);
@@ -46,30 +30,28 @@ begin
 	process(OSC_FPGA, PB)
 	begin
 		if (rising_edge(OSC_FPGA)) then
+
+			-- Synch the SPI clock
+			sck_synchronizer(2 downto 1) <= sck_synchronizer(1 downto 0);
+			sck_synchronizer(0) <= SYS_SPI_SCK;
 		
-			-- Synch the PB
+			-- Synch the pushbutton
 			pb0_synchronizer(2 downto 1) <= pb0_synchronizer(1 downto 0);
 			pb0_synchronizer(0) <= PB(0);
 		
 			if pb0_synchronizer(2 downto 1) = "01" then
 				-- Rising edge is button release
-				--spi_counter <= "000";
 				spi_value <= X"C5"; -- 11000101
 				spi_readvalue <= X"00";
 				SYS_SPI_MISO <= '0'; -- Mode1 means this value should never get sampled
 				LED(1) <= '0';
-  			   --spi_value <= spi_value(6 downto 0) & '0';
 				--sck_synchronizer <= "000";
 			else
 				
-				-- Synch the SPI clock
-				sck_synchronizer(2 downto 1) <= sck_synchronizer(1 downto 0);
-				sck_synchronizer(0) <= SYS_SPI_SCK;
 		
 				-- SPI MODE 1: clock rests low, assert on rise, latch on fall.
 				if (sck_synchronizer(2 downto 1) = "01") then
 					-- rise: assert
-					--spi_counter <= spi_counter + 1;
 					spi_value <= spi_value(6 downto 0) & '0';
 					SYS_SPI_MISO <= spi_value(7);
 				else
